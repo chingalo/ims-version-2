51420c0caacd81cf9d564651aa53c99c7bba1d45
chingalo <profschingalo@gmail.com>
2015-01-10 01:14:05 +0300

    start handling go requests for sign in and sign up for new redisgn of the system

--- manageuser/views.py
+++ manageuser/views.py
@@ -3,8 +3,7 @@ from django.shortcuts import render_to_response, get_object_or_404, render
 from django.core.context_processors import csrf
 from django.core.urlresolvers import reverse
 import json 
-from django.core.mail import send_mail
-from project.models import *
+from django.core.mail import *
 from random import randrange
 from datetime import datetime
 # time   str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
@@ -16,6 +15,8 @@ from reportlab.lib.enums import TA_CENTER
 from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+
+from project.models import *
 
 
 #home page for the system when loading
