5cd77721460ab349c925da391f54f241fdc08690
chingalo <profschingalo@gmail.com>
2015-01-10 01:11:52 +0300

    start handling go requests for sign in and sign up for new redisgn of the system

--- manageuser/urls.py
+++ manageuser/urls.py
@@ -4,5 +4,7 @@ from manageuser import views
 
 urlpatterns = patterns('',
     url(r'^$', views.home_page, name='home_page'),
+    url(r'^signin/$', views.signin, name='signin'),
+    url(r'^signup/$', views.signup, name='signup'),
    
 )
--- manageuser/views.py
+++ manageuser/views.py
@@ -1,10 +1,39 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect,HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404, render
+from django.core.context_processors import csrf
+from django.core.urlresolvers import reverse
+import json 
+from django.core.mail import send_mail
+from project.models import *
+from random import randrange
+from datetime import datetime
+# time   str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+from reportlab.pdfgen import canvas
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.lib.enums import TA_CENTER
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 
 
-
+#home page for the system when loading
 def home_page(request):
 
-	n = 1999
-
-	context = {'captureValue': n}
+	context = {}
 	return render(request,'home_page.html',context)
+
+
+#to handle all requests for sign in into the system
+def signin(request):
+
+	context = {}
+	return render(request, 'base.html', context)
+
+
+#to hndle all requests for new user to be registered into new system
+def signup(request):
+
+	context = {}
+	return render(request, 'base.html', context)	
