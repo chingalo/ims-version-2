be3fd054d7213c12a6e93b126a8c8cbe0c3cc1e0
chingalo <profschingalo@gmail.com>
2015-02-23 16:11:42 +0300

    add pop up modal for all actions on a given project

--- reportissues/urls.py
+++ reportissues/urls.py
@@ -9,16 +9,13 @@ urlpatterns = patterns('',
     url(r'', include('manageuser.urls')),
     url(r'', include('manageproject.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT,
+        'show_indexes': True}),
+    url(r'',
+        include('django.contrib.staticfiles.urls')),
     
     
 )+ staticfiles_urlpatterns()
 
-if settings.DEBUG:
-    urlpatterns = patterns('',
-		url(r'^', include('project.urls')),
-	url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': settings.MEDIA_ROOT,
-        'show_indexes': True}),
-        url(r'',
-        include('django.contrib.staticfiles.urls')),
-    ) + urlpatterns
+
