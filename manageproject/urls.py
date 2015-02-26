af631cd55a2def870ba760856cff3bb3b2d5fb43
chingalo <profschingalo@gmail.com>
2015-02-26 21:02:02 +0300

    complete delete the project functionality

--- manageproject/urls.py
+++ manageproject/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     url(r'^all-projects/(?P<user_id>\d+)$', views.viewAllProject, name='viewAllProject'),
     url(r'^add-project/(?P<user_id>\d+)$', views.addProject, name='addProject'),
     url(r'^edit-project/(?P<user_id>\d+)/(?P<project_id>\d+)$', views.editProject, name='editProject'),
+    url(r'^delete-project/(?P<user_id>\d+)/(?P<project_id>\d+)$', views.deleteProject, name='deleteProject'),
     url(r'^project/(?P<user_id>\d+)/(?P<project_id>\d+)$', views.viewProject, name='viewProject'),
    
 )
